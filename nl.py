2a1973775205253ef99a258fe71e4cec77269099
e413147 <lauren.kimpel@lmco.com>
2023-05-03 11:00:47 -0400

    initial commit

---
+++ nl.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import permutation_test, manwhitneyu, chisquare
+
+def calc_latency_rank(df):
+    '''
+    Ranks the nuclear latency of a particular country based on sums of boolean data
+    '''
+
+    # first, get all of the data from a particular country
+
+    # then sum this data; higher sums = higher latency.
+
+    # append the latency ranking calculation to the dataset
+    return 
+
+if __name__=="__main__":
+    df = pd.read_excel("nl_dataset_v.1.2.xlsx")
+    print(df)
+
+
+    # compute the U-sample statistics
+
+    # perform permutation tests over these statistics
+
+    # perform chi-square tests over these statistics 
